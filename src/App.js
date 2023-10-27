c797eaf543392bc9927c06c6e2d485a77d3e70d1
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-10-27 16:36:02 -0300

    Set Template with greeting

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import store from './redux/store';
-import Greeting from './components/Greetings';
+import Layout from './components/Layout';
+import About from './routes/About';
+import Login from './routes/Login';
+import Profile from './routes/Profile';
+import NotMatch from './routes/NotMatch';
+import Home from './routes/Home';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Greeting />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotMatch />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
---
+++ src/routes/Home.jsx
@@ -0,0 +1,10 @@
+import Greeting from '../components/Greetings';
+import Header from '../components/Header';
+
+const Home = () => (
+  <div>
+    <Header />
+    <Greeting />
+  </div>
+);
+export default Home;
